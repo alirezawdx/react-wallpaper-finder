5128b577b2565052a5a76e25f616ffcbe2d2fc78
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2023-05-27 12:33:58 +0330

    changing submit button & change app.js component to be more readable

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import './assets/css/style.css';
-import React, { useState } from 'react';
-import SearchBox from './components/SearchBox';
-import ImageList from './components/ImageList';
-import Loader from './components/Loader';
-import Alert from './components/Alert';
-import getImagesByQueryText from './api';
+import "./assets/css/style.css";
+import React, { useState } from "react";
+import SearchBox from "./components/SearchBox";
+import ImageList from "./components/ImageList";
+import Loader from "./components/Loader";
+import Alert from "./components/Alert";
+import getImagesByQueryText from "./api";
 
 function App() {
-	const [images, setImages] = useState([]);
-	const [errorMessage, setErrorMessage] = useState('');
+    const [images, setImages] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
     const [loading, setLoading] = useState(false);
 
-	async function getImagesOnSubmit(queryText) {
-		setLoading(true);
-		setImages([]);
-		setErrorMessage('');
-		try {
-			if (!queryText) throw new Error('Please enter something to search');
-			const { results } = await getImagesByQueryText(
-				queryText
-			);
-			setImages(results);
-		} catch (error) {
-			setErrorMessage(error.message);
-		} finally {
-			setLoading(false);
-		}
-	}
+    async function getImagesOnSubmit(queryText) {
+        setLoading(true);
+        setImages([]);
+        setErrorMessage("");
+        try {
+            if (!queryText) throw new Error("Please enter something to search");
+            const { results } = await getImagesByQueryText(queryText);
+            setImages(results);
+        } catch (error) {
+            setErrorMessage(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // I think this way would be better and more readable way for App.js component
+    let content = "";
+    if (loading) {
+        content = <Loader />;
+    } else if (errorMessage) {
+        content = <Alert type="danger" message={errorMessage} />;
+    } else {
+        content = <ImageList images={images} />;
+    }
 
     return (
-		<div className='container'>
-			<h1>React Wallpaper Finder</h1>
-			<SearchBox onSubmit={getImagesOnSubmit} />
-			{loading && <Loader />}
-			{errorMessage && <Alert type='danger' message={errorMessage} />}
-			{images && !errorMessage && !loading && (
-				<ImageList images={images} />
-			)}
-		</div>
-	);
+        <div className="container">
+            <h1>React Wallpaper Finder</h1>
+            <SearchBox onSubmit={getImagesOnSubmit} />
+            {content}
+        </div>
+    );
 }
 
 export default App;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,34 +1,42 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function SearchBox({ onSubmit }) {
-	const [queryText, setQueryText] = useState('');
+    const [queryText, setQueryText] = useState("");
 
-	function handleChange(event) {
-		setQueryText(event.target.value);
-	}
+    function handleChange(event) {
+        setQueryText(event.target.value);
+    }
 
-	function handleFormSubmit(event) {
-		// Stop refreshing the page after submitting the search queryText
-		event.preventDefault();
+    function handleFormSubmit(event) {
+        // Stop refreshing the page after submitting the search queryText
+        event.preventDefault();
 
-		// Then send the search queryText to the parent component
-		onSubmit(queryText);
-	}
+        // Then send the search queryText to the parent component
+        onSubmit(queryText);
+    }
 
-	return (
-		<div className='search-box'>
-			<form onSubmit={handleFormSubmit}>
-				<input
-					type='text'
-					autoFocus
-					value={queryText}
-					onChange={handleChange}
-					placeholder='Search...'
-				/>
-				<input type='Submit' value='Search' />
-			</form>
-		</div>
-	);
+    return (
+        <div className="search-box">
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    autoFocus
+                    value={queryText}
+                    onChange={handleChange}
+                    placeholder="Search..."
+                />
+                {/* I got this warning in browser console */}
+
+                {/* warning: You provided a value prop to a form field without an
+                onChange handler. This will render a read-only field. If the
+                field should be mutable use defaultValue. Otherwise, set either
+                onChange or readOnly. */}
+
+                {/* so I changed submit input to button submit */}
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
 }
 
 export default SearchBox;
